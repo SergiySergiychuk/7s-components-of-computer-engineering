5453943c589bf88e31a264fa2107001b8722d3e3
Serhii Serhiychuk <kpi.fpm.serhiychuk@gmail.com>
2021-10-21 11:52:15 +0300

    fix

--- lab1/client/client.py
+++ lab1/client/client.py
@@ -7,20 +7,18 @@ def get_text_checksum(text):
     return hashlib.md5(text.encode()).hexdigest()
 
 if(len(sys.argv)==3):
-    print('============================')
     ip = sys.argv[1]
     port = sys.argv[2]
     response = requests.get(f'http://{ip}:{port}')
     original_checksum = response.headers['checksum']
     file_content = response.text
-    print(response.headers)
-
     if original_checksum==get_text_checksum(file_content):
         filename = generate()
         f = open(f'clientdata/{filename}', 'a')
         f.write(str(file_content))
         f.close()
         print(f'Success! File {filename} was created')
+        print('Checksum is correct!')
     else:
         print('Checksum is wrong!')
 
